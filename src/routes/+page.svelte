d369ece2c1a49667840b205c57e539649e30d24c
Nik <niklas@kizo-agency.com>
2024-10-22 13:26:14 +0200

    Create frontend integration for one-off button and install packages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,22 @@
+<script>
+	import { PUBLIC_STRIPE_KEY } from '$env/static/public';
+	import { loadStripe } from '@stripe/stripe-js';
+
+	async function onclick() {
+		const stripe = await loadStripe(PUBLIC_STRIPE_KEY);
+
+		const response = fetch('/api/checkout', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({ priceId: 'price_1QCg9pGALH1mrUSjuIBIPDpV' })
+		});
+	}
+</script>
+
 <main>
-	<button class="cta"> Purchase now! </button>
+	<button {onclick} class="cta"> Purchase now! </button>
 </main>
 
 <style>
