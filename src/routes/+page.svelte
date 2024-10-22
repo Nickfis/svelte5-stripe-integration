eb529b21288152ac3b5555179d2ee28dd7310b25
Nik <niklas@kizo-agency.com>
2024-10-22 13:19:58 +0200

    QUick styling of button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<main>
+	<button class="cta"> Purchase now! </button>
+</main>
+
+<style>
+	main {
+		height: 100vh;
+		width: 100vw;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+
+	button {
+		width: 300px;
+		height: 50px;
+		color: white;
+		background-color: black;
+		cursor: pointer;
+	}
+</style>
